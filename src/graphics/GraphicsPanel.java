11fbea4be14631d886ea068c56c312fd93c1d488
TheAIBot <andreasuranus@hotmail.com>
2018-01-04 15:50:49 +0100

    now draws tanks

--- src/graphics/GraphicsPanel.java
+++ src/graphics/GraphicsPanel.java
@@ -1,16 +1,50 @@
 package graphics;
 
+import java.awt.Color;
 import java.awt.Graphics;
+import java.util.ArrayList;
 
 import javax.swing.JPanel;
 
+import engine.Bullet;
+import engine.Tank;
+
 public class GraphicsPanel extends JPanel {
+	ArrayList<Tank> tanks = new ArrayList<Tank>();
+	ArrayList<Bullet> bullets = new ArrayList<Bullet>();
+	
 
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		
+		drawTanks(g);
+		drawBullets(g);
+	}
+	
+	private void drawTanks(Graphics g)
+	{
+		for (Tank tank : tanks) {
+			drawTank(tank, g);
+		}
+	}
+	
+	private void drawTank(Tank tank, Graphics g)
+	{
 		
+	}
+	
+	private void drawBullets(Graphics g)
+	{
+		for (Bullet bullet : bullets) {
+			drawBullet(bullet, g);
+		}
+	}
+	
+	private void drawBullet(Bullet bullet, Graphics g)
+	{
+		g.setColor(Color.RED);
+		g.drawOval((int)(bullet.x - bullet.width / 2), (int)(bullet.y- bullet.height / 2), (int)bullet.width, (int)bullet.height);
 	}
 	
 }
