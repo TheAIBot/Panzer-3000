87010b55e1fccba96a0e451e870c19ddfae03671
Jesper <jesper.birch3@gmail.com>
2018-01-11 15:50:44 +0100

    meh

--- src/network/spaces/PeerServerConnector.java
+++ src/network/spaces/PeerServerConnector.java
@@ -1,12 +1,16 @@
 package network.spaces;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 import org.jspace.SequentialSpace;
 
+import com.sun.java.swing.plaf.gtk.resources.gtk_it;
+
 public class PeerServerConnector extends SuperServerConnector{
 
-	public final int RANDOM_STRING_LENGTH = 12;
+	private final String SALTCHARS = "abcdefghijklmnopqrstuvqxyzæøå0123456789";
+	public  final int RANDOM_STRING_LENGTH = 12;
 	
 	@Override
 	public void run() {
@@ -20,21 +24,29 @@ public class PeerServerConnector extends SuperServerConnector{
 		String[][] privateIDTuples = createPrivateIDs(numClients);
 	}
 
-	private String[][] createPrivateIDs(int numClients) {
+	private ArrayList<String>[] createPrivateIDs(int numClients) {
 		//A complete graph is made:
-		String[][] graphOfRandomStrings = new String[numClients][numClients-1];
+		ArrayList<String>[] graphOfRandomStrings = (ArrayList<String>[]) new ArrayList[numClients];
 		Random random = new Random();
 		//The random strings linked to the edges.
 		for (int i = 0; i < graphOfRandomStrings.length; i++) {
-			for (int j = 0; j < graphOfRandomStrings[i].length; j++) {
-				String randomString = "";				
-				for (int k = 0; k < RANDOM_STRING_LENGTH; k++) {
-					randomString += (char) (random.nextInt() + 1);
-				}
-				
+			for (int j = i; j < graphOfRandomStrings[i].size(); j++) {
+				String randomString = getRandomString();
+				graphOfRandomStrings[i].add(randomString);
+				graphOfRandomStrings[j].add(randomString);
 			}
 		}
-		
+		return graphOfRandomStrings;
 	}
 
+	
+	private String getRandomString() {
+		StringBuilder salt = new StringBuilder();
+        Random rnd = new Random();
+        while (salt.length() < RANDOM_STRING_LENGTH) { // length of the random string.
+            int index = rnd.nextInt(SALTCHARS.length());
+            salt.append(SALTCHARS.charAt(index));
+        }
+        return salt.toString();
+	}
 }
