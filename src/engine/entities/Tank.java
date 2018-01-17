9f93bf66fd773205eae5e7f781cab9c98935c7a8
TheAIBot <andreasuranus@hotmail.com>
2018-01-17 10:34:05 +0100

    asda

--- src/engine/entities/Tank.java
+++ src/engine/entities/Tank.java
@@ -34,7 +34,7 @@ public class Tank extends DeSerializer {
 	public static final double 	GUN_HEIGHT 			= 0.005;
 	public static final int 	TANK_HEALTH 		= 100;
 	public static final double	SCALAR 				= 10000;
-	public static final int 	TIME_BETWEEN_SHOTS 	= 70;
+	public static final int 	TIME_BETWEEN_SHOTS 	= 55;
 	public static final double 	TURNING_ANGLE 		= 1.15;
 	
 	public Tank(double xNew, double yNew, double bodyAngleNew, double gunAngleNew, int idNew, String username) {
